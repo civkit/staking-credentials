6acabf1d2b67adec6fb090f04319faa4dfbf5020
Antoine Riard <dev@ariard.me>
2023-01-25 01:59:27 +0000

    Add basic CommitManager

---
+++ lightning-htlc-scorer/src/issuance/commitmanager.rs
@@ -0,0 +1,34 @@
+// This file is Copyright its original authors, visibile in version control
+// history.
+//
+// This file is licensed under the Apache License, Version 2.0 <LICENSE-APACHE
+// or http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
+// You may not use this file except in accordance with one or both of these
+// licenses.
+
+
+/// Configuration we set when applicable.
+#[derive(Copy, Clone, Debug)]
+pub struct RiskEngineConfig {}
+
+impl Default for RiskEngineConfig {
+	fn default() -> RiskEngineConfig {
+		RiskEngineConfig {}
+	}
+}
+
+/// Manager which stores asset proofs and generate credentials generation requests.
+/// TODO: should the authenticated credentials be considered as valuable digital
+/// collectibles like bitcoin secret keys and as such firewall behind signer interface ?
+pub struct CommitManager {
+	default_configuration: RiskEngineConfig,
+}
+
+impl CommitManager {
+	pub fn new() -> Self {
+		CommitManager {
+			default_configuration: RiskEngineConfig::default(),
+		}
+	}
+}
